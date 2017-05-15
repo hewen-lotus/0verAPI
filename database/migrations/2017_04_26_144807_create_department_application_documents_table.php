0fed9f8363b728929b217a03693113fdd981386b
sunnyworm <sunnyworm@gmail.com>
2017-05-15 22:26:41 +0800

    增加 department_application_documents 的歷史紀錄表

--- database/migrations/2017_04_26_144807_create_department_application_documents_table.php
+++ database/migrations/2017_04_26_144807_create_department_application_documents_table.php
@@ -14,10 +14,10 @@ class CreateDepartmentApplicationDocumentsTable extends Migration
     public function up()
     {
         Schema::create('department_application_documents', function (Blueprint $table) {
-            $table->string('dept_id');
-            $table->unsignedInteger('document_type_id');
-            $table->string('detail');
-            $table->string('eng_detail');
+            $table->string('dept_id')->comment('系所代碼');
+            $table->unsignedInteger('document_type_id')->comment('備審資料代碼（系統自動產生）');
+            $table->string('detail')->comment('詳細說明');
+            $table->string('eng_detail')->comment('英文的詳細說明');
             $table->string('created_at');
             $table->string('updated_at');
             $table->string('deleted_at')->nullable();
@@ -27,6 +27,46 @@ class CreateDepartmentApplicationDocumentsTable extends Migration
         Schema::table('department_application_documents', function (Blueprint $table) {
             $table->foreign('document_type_id')->references('id')->on('application_document_types');
             $table->foreign('dept_id')->references('id')->on('department_data');
+        });
+
+        Schema::create('department_saved_application_documents', function (Blueprint $table) {
+            $table->increments('history_id');
+            $table->string('dept_id')->comment('系所代碼');
+            $table->foreign('dept_id')->references('id')->on('department_data');
+            $table->unsignedInteger('document_type_id')->comment('備審資料代碼（系統自動產生）');
+            $table->foreign('document_type_id')->references('id')->on('application_document_types');
+            $table->string('detail')->nullable()->comment('詳細說明');
+            $table->string('eng_detail')->nullable()->comment('英文的詳細說明');
+            $table->string('modified_by')->comment('按下儲存的人是誰');
+            $table->foreign('modified_by')->references('username')->on('users');
+            $table->string('ip_address')->comment('按下儲存的人的IP');
+            $table->string('created_at');
+            $table->string('updated_at');
+            $table->string('deleted_at')->nullable();
+        });
+
+        Schema::create('department_committed_application_documents', function (Blueprint $table) {
+            $table->increments('history_id');
+            $table->string('dept_id')->comment('系所代碼');
+            $table->foreign('dept_id')->references('id')->on('department_data');
+            $table->unsignedInteger('document_type_id')->comment('備審資料代碼（系統自動產生）');
+            $table->foreign('document_type_id', 'department_committed_application_documents_doc_type_id_foreign')->references('id')->on('application_document_types');
+            $table->string('detail')->nullable()->comment('詳細說明');
+            $table->string('eng_detail')->nullable()->comment('英文的詳細說明');
+            $table->string('committed_by')->comment('按下送出的人是誰');
+            $table->foreign('committed_by')->references('username')->on('users');
+            $table->string('ip_address')->comment('按下送出的人的IP');
+            $table->string('review_status')->comment('waiting|confirmed|editing');
+            $table->string('reason')->nullable()->comment('讓學校再次修改的原因');
+            $table->string('replied_by')->comment('海聯回覆的人員');
+            $table->foreign('replied_by')->references('username')->on('admins');
+            $table->string('replied_at')->comment('海聯回覆的時間點');
+            $table->string('confirmed_by')->comment('海聯審查的人員');
+            $table->foreign('confirmed_by')->references('username')->on('admins');
+            $table->string('confirmed_at')->comment('海聯審查的時間點');
+            $table->string('created_at');
+            $table->string('updated_at');
+            $table->string('deleted_at')->nullable();
         });
     }
 
@@ -42,6 +82,22 @@ class CreateDepartmentApplicationDocumentsTable extends Migration
             $table->dropForeign('department_application_documents_dept_id_foreign');
         });
 
+        Schema::table('department_saved_application_documents', function (Blueprint $table) {
+            $table->dropForeign('department_saved_application_documents_document_type_id_foreign');
+            $table->dropForeign('department_saved_application_documents_dept_id_foreign');
+            $table->dropForeign('department_saved_application_documents_modified_by_foreign');
+        });
+
+        Schema::table('department_committed_application_documents', function (Blueprint $table) {
+            $table->dropForeign('department_committed_application_documents_doc_type_id_foreign');
+            $table->dropForeign('department_committed_application_documents_dept_id_foreign');
+            $table->dropForeign('department_committed_application_documents_committed_by_foreign');
+            $table->dropForeign('department_committed_application_documents_replied_by_foreign');
+            $table->dropForeign('department_committed_application_documents_confirmed_by_foreign');
+        });
+
+        Schema::dropIfExists('department_committed_application_documents');
+        Schema::dropIfExists('department_saved_application_documents');
         Schema::dropIfExists('department_application_documents');
     }
 }
