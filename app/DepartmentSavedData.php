2e17fdb59caa3d773249299dc8b0bd19297a45b1
richegg <zxp86021@gmail.com>
2017-05-17 16:37:36 +0800

    更新 DepartmentData 系列 Model 欄位名稱

--- app/DepartmentSavedData.php
+++ app/DepartmentSavedData.php
@@ -109,37 +109,37 @@ class DepartmentSavedData extends Model
         'card_code', //讀卡代碼
         'title', //系所名稱
         'eng_title', //系所英文名稱
-        'choice_memo', //選系說明
-        'eng_choice_memo', //選系英文說明
-        'doc_memo', //書審說明
-        'eng_doc_memo', //書審英文說明
-        'dept_memo', //備註
-        'eng_dept_memo', //英文備註
+        'description', //選系說明
+        'eng_description', //選系英文說明
+        'memo', //給海聯的備註
+        'eng_memo', //給海聯的英文備註
         'url', //系網站網址
         'eng_url', //英文系網站網址
-        'last_year_offer', //去年聯合分發錄取名額
-        'decrease_reason', //減招原因
-        'last_year_amount', //去年聯合分發名額（只有學士班有聯合分發）
-        'amount', //聯合分發名額（只有學士班有聯合分發）
-        'personal_apply_amount', //個人申請名額
-        'last_year_personal_apply_amount', //去年個人申請名額
+        'last_year_admission_placement_amount', //去年聯合分發錄取名額
+        'last_year_admission_placement_quota', //去年聯合分發名額（只有學士班有聯合分發）
+        'admission_placement_quota', //聯合分發名額（只有學士班有聯合分發）
+        'decrease_reason_of_admission_placement', //聯合分發人數減招原因
         'last_year_personal_apply_offer', //去年個人申請錄取名額
-        'self_recurit', //是否有自招
-        'self_recurit_amount', //自招名額
-        'special_class', //是否招收僑生專班
-        'special_class_foriegn', //是否招收外生專班
-        'special_dept', //特殊系所（醫、牙、中醫、藝術）
-        'sex_limit', //性別限制
-        'ratify', //核定名額
+        'last_year_personal_apply_amount', //'去年個人申請名額
+        'admission_selection_quota', //個人申請名額
+        'has_self_enrollment', //是否有自招
+        'self_enrollment_quota', //自招名額
+        'has_special_class', //是否招收僑生專班
+        'has_foreign_special_class', //是否招收外生專班
+        'special_dept_type', //特殊系所（醫、牙、中醫、藝術）
+        'gender_limit', //性別限制
+        'admission_placement_ratify_quota', //教育部核定聯合分發名額
+        'admission_selection_ratify_quota', //教育部核定個人申請名額
         'rank', //志願排名
-        'sortorder', //輸出排序
-        'after_birth', //限…之後出生
-        'before_birth', //限…之前出生
-        'dept_group', //18大學群代碼
-        'sub_dept_group', //次要18大學群代碼
-        'eng_taught', //全英文授課
-        'disabilities', //是否招收身障學生
-        'BuHweiHwaWen', //是否招收不具華文基礎學生
+        'sort_order', //輸出排序
+        'has_birth_limit', //是否限制出生日期
+        'birth_limit_after', //限…之後出生
+        'birth_limit_before', //限…之前出生
+        'main_group', //主要隸屬學群代碼
+        'sub_group', //次要隸屬學群代碼
+        'has_eng_taught', //全英文授課
+        'has_disabilities', //是否招收身障學生
+        'has_BuHweiHwaWen', //是否招收不具華文基礎學生
         'evaluation', //系所評鑑等級
         'modified_by', //儲存資料的人是誰
         'quantity_modified_by', //儲存名額的人是誰
