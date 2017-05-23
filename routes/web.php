afad299b57f1342c2efd31cdb1d64f2630eb7e0a
richegg <zxp86021@gmail.com>
2017-05-23 13:21:22 +0800

    把 route 從 api 搬到 web 新增 MaintenanceMode 的 json response

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,26 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/users/login', 'Auth\LoginController@UserLoginStatus');
+
+Route::post('/users/login', 'Auth\LoginController@UserLogin');
+
+Route::post('/users/logout', 'Auth\LoginController@UserLogout');
+
+Route::resource('/schools.editors', 'SchoolEditorController');
+
+Route::resource('/schools.histories', 'SchoolHistoryController');
+
+Route::resource('/schools.systems.histories', 'SystemHistoryController');
+
+Route::resource('/schools.systems.departments.histories', 'DepartmentHistoryController');
+
+Route::post('/personal-and-priority-data-importer', 'PersonalAndPriorityDataImportController@import');
+
+Route::get('/db-schema-to-md', 'DBSchemaToMDController@export');
+
+Route::get('/limesurvey-filemtime', 'LimesurveyFilemtimeController@export');
+
+Route::post('/csv-to-seeder', 'CSVToSeederController@import');
+
