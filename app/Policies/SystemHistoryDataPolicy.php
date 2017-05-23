e043c17bbfba3be79ed5215c592bd9c7375d1f4f
richegg <zxp86021@gmail.com>
2017-05-23 22:32:29 +0800

    update response after login

--- app/Policies/SystemHistoryDataPolicy.php
+++ app/Policies/SystemHistoryDataPolicy.php
@@ -11,26 +11,73 @@ class SystemHistoryDataPolicy
     use HandlesAuthorization;
 
     /**
-     * Determine whether the user can view the systemHistoryData.
+     * Determine whether the user can view the systemHistoryData quota.
      *
      * @param  \App\User  $user
      * @param  \App\SystemHistoryData  $systemHistoryData
      * @return mixed
      */
-    public function view(User $user, SystemHistoryData $systemHistoryData)
+    public function view_quota(User $user, SystemHistoryData $systemHistoryData, $school_id)
     {
-        //
+        if ($user->school_editor != NULL) {
+            if (($user->school_editor->school_code == $school_id || $school_id == 'me') && $histories_id == 'latest') {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     /**
-     * Determine whether the user can create systemHistoryDatas.
+     * Determine whether the user can view the systemHistoryData info.
+     *
+     * @param  \App\User  $user
+     * @param  \App\SystemHistoryData  $systemHistoryData
+     * @return mixed
+     */
+    public function view_info(User $user, SystemHistoryData $systemHistoryData, $school_id)
+    {
+        if ($user->school_editor != NULL) {
+            if (($user->school_editor->school_code == $school_id || $school_id == 'me') && $histories_id == 'latest') {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Determine whether the user can create systemHistoryDatas quota.
      *
      * @param  \App\User  $user
      * @return mixed
      */
-    public function create(User $user)
+    public function create_quota(User $user)
     {
-        //
+        if ($user->school_editor != NULL) {
+            if (($user->school_editor->school_code == $school_id || $school_id == 'me') && $histories_id == 'latest') {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Determine whether the user can create systemHistoryDatas info.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function create_info(User $user)
+    {
+        if ($user->school_editor != NULL) {
+            if (($user->school_editor->school_code == $school_id || $school_id == 'me') && $histories_id == 'latest') {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     /**
