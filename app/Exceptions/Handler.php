b58d873b253b393ed67304595d967393fe4b68c7
richegg <zxp86021@gmail.com>
2017-05-24 14:02:59 +0800

    update except message to json

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -5,6 +5,8 @@ namespace App\Exceptions;
 use Exception;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use App;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class Handler extends ExceptionHandler
 {
@@ -44,7 +46,34 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
-        return parent::render($request, $exception);
+        $response = [
+            'messages' => ['Sorry, something went wrong.']
+        ];
+
+        // If the app is in debug mode and not production
+        if (config('app.debug') && !App::environment('production'))
+        {
+            // Add the exception class name, message and stack trace to response
+            $response['exception'] = get_class($exception); // Reflection might be better here
+            $response['message'] = $exception->getMessage();
+            $response['trace'] = $exception->getTrace();
+        }
+
+        // Default response of 400
+        $status = 400;
+
+        // If this exception is an instance of HttpException
+        if ($this->isHttpException($exception))
+        {
+            // Grab the HTTP status code from the Exception
+            $status = $exception->getStatusCode();
+        }
+
+        // Return a JSON response with the response array and status code
+        return response()->json($response, $status);
+
+        // Default to the parent class' implementation of handler
+        //return parent::render($request, $exception);
     }
 
     /**
