e0a60366094bb73a26cc0ef87b44d04fb0120642
sunnyworm <sunnyworm@gmail.com>
2017-05-26 10:29:26 +0800

    增加二技資料與二技歷史資料的 Seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -33,6 +33,8 @@ class DatabaseSeeder extends Seeder
             $this->call(DepartmentHistoryDataTableSeeder::class);
             $this->call(GraduateDepartmentDataTableSeeder::class);
             $this->call(GraduateDepartmentHistoryDataTableSeeder::class);
+            $this->call(TechDepartmentDataTableSeeder::class);
+            $this->call(TechDepartmentHistoryDataTableSeeder::class);
         }
     }
 }
