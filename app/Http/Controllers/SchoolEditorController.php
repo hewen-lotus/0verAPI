6f06f82886546b9978e7fbe5ba98fcc0e5a7888b
richegg <zxp86021@gmail.com>
2017-06-02 16:29:04 +0800

    add middlewarw to SchoolEditorController

--- app/Http/Controllers/SchoolEditorController.php
+++ app/Http/Controllers/SchoolEditorController.php
@@ -20,7 +20,7 @@ class SchoolEditorController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware(['auth', 'switch']);
     }
 
     /**
