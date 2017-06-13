3289b3a34c551dcc883cdef3ab4a2b4ed38021e1
richegg <zxp86021@gmail.com>
2017-06-13 14:25:06 +0800

    add new controller composer update

--- app/Http/Controllers/DepartmentHistoryDataController.php
+++ app/Http/Controllers/DepartmentHistoryDataController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class TwoYearTechDepartmentDataController extends Controller
+class DepartmentHistoryDataController extends Controller
 {
     //
 }
