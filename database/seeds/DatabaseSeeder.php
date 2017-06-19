9e12e49fd00fea8a3cf38720d218dd57bc8daefa
sunnyworm <sunnyworm@gmail.com>
2017-06-19 14:59:48 +0800

    增加「已可招收中五生與已可自招之學校」的 Seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -35,6 +35,7 @@ class DatabaseSeeder extends Seeder
             $this->call(GraduateDepartmentHistoryDataTableSeeder::class);
             $this->call(TechDepartmentDataTableSeeder::class);
             $this->call(TechDepartmentHistoryDataTableSeeder::class);
+            $this->call(SchoolLastYearSelfEnrollmentAndFiveYearStatusTableSeeder::class);
         }
     }
 }
