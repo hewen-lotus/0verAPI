ac10ea69fc33d0833b00a4d99ca6b6d06af8784f
richegg <zxp86021@gmail.com>
2017-06-27 20:56:17 +0800

    修正 DB 修改方式

---
+++ database/migrations/2017_06_27_204759_update_application_documents_table_dept_id_foreign_in_two_year_tech_dept_and_graduate_dept.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateApplicationDocumentsTableDeptIdForeignInTwoYearTechDeptAndGraduateDept extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('graduate_dept_application_docs', function (Blueprint $table) {
+            $table->dropForeign('graduate_dept_application_docs_dept_id_foreign');
+            $table->foreign('dept_id')->references('id')->on('graduate_department_data');
+        });
+
+        Schema::table('graduate_dept_history_application_docs', function (Blueprint $table) {
+            $table->dropForeign('graduate_dept_history_application_docs_dept_id_foreign');
+            $table->dropForeign('graduate_dept_history_application_docs_history_id_foreign');
+            $table->foreign('dept_id')->references('id')->on('graduate_department_data');
+            $table->foreign('history_id')->references('history_id')->on('graduate_department_history_data');
+        });
+
+        Schema::table('two_year_tech_dept_application_docs', function (Blueprint $table) {
+            $table->dropForeign('two_year_tech_dept_application_docs_dept_id_foreign');
+            $table->foreign('dept_id')->references('id')->on('two_year_tech_department_data');
+        });
+
+        Schema::table('two_year_tech_dept_history_application_docs', function (Blueprint $table) {
+            $table->dropForeign('two_year_tech_dept_history_application_docs_dept_id_foreign');
+            $table->foreign('dept_id')->references('id')->on('two_year_tech_department_data');
+            $table->dropForeign('two_year_tech_dept_history_application_docs_history_id_foreign');
+            $table->foreign('history_id')->references('history_id')->on('two_year_tech_department_history_data');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
