7481edfdaa28274cf42bc7baf3f0059006113e42
richegg <zxp86021@gmail.com>
2017-08-16 10:57:14 +0800

    更新系統功能開關

--- app/Http/Middleware/AppSwitch.php
+++ app/Http/Middleware/AppSwitch.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Contracts\Foundation\Application;
 
 use App\AppSwitchData;
 
@@ -13,16 +14,21 @@ use Log;
 class AppSwitch
 {
     /**
+     * The application implementation.
+     *
+     * @var \Illuminate\Contracts\Foundation\Application
+     */
+    protected $app;
+
+    /**
      * Create a new middleware instance.
      *
+     * @param  \Illuminate\Contracts\Foundation\Application  $app
      * @return void
      */
-    public function __construct()
+    public function __construct(Application $app)
     {
-        // 白名單的 function
-        $this->whiteList = collect([
-
-        ]);
+        $this->app = $app;
     }
 
     /**
@@ -36,9 +42,11 @@ class AppSwitch
     {
         $method = Route::currentRouteAction();
 
-        $app_switch_data = new AppSwitchData();
+        Log::info($method);
 
-        if ( $this->whiteList->contains($method) ) {
+        if ( $this->app->environment() != 'local' ) {
+            $app_switch_data = new AppSwitchData();
+
             $db_now = DB::raw('NOW()');
 
             if ( !$app_switch_data->where([
@@ -51,8 +59,6 @@ class AppSwitch
                 ], 503);
             }
         }
-
-        Log::info($method);
 
         return $next($request);
     }
