6512265b78470e1f92ee4e1c5596ea368cf94cd0
richegg Tsai <zxp86021@users.noreply.github.com>
2017-09-04 20:49:44 +0800

    英文資料預設為提供

--- database/migrations/2017_08_31_112416_add_eng_data_use_or_not_to_all_dapartment_table.php
+++ database/migrations/2017_08_31_112416_add_eng_data_use_or_not_to_all_dapartment_table.php
@@ -14,27 +14,27 @@ class AddEngDataUseOrNotToAllDapartmentTable extends Migration
     public function up()
     {
         Schema::table('department_data', function (Blueprint $table) {
-            $table->boolean('use_eng_data')->default(false)->comment('本年度是否提供英文資料');
+            $table->boolean('use_eng_data')->default(true)->comment('本年度是否提供英文資料');
         });
 
         Schema::table('department_history_data', function (Blueprint $table) {
-            $table->boolean('use_eng_data')->default(false)->comment('本年度是否提供英文資料');
+            $table->boolean('use_eng_data')->default(true)->comment('本年度是否提供英文資料');
         });
 
         Schema::table('two_year_tech_department_data', function (Blueprint $table) {
-            $table->boolean('use_eng_data')->default(false)->comment('本年度是否提供英文資料');
+            $table->boolean('use_eng_data')->default(true)->comment('本年度是否提供英文資料');
         });
 
         Schema::table('two_year_tech_department_history_data', function (Blueprint $table) {
-            $table->boolean('use_eng_data')->default(false)->comment('本年度是否提供英文資料');
+            $table->boolean('use_eng_data')->default(true)->comment('本年度是否提供英文資料');
         });
 
         Schema::table('graduate_department_data', function (Blueprint $table) {
-            $table->boolean('use_eng_data')->default(false)->comment('本年度是否提供英文資料');
+            $table->boolean('use_eng_data')->default(true)->comment('本年度是否提供英文資料');
         });
 
         Schema::table('graduate_department_history_data', function (Blueprint $table) {
-            $table->boolean('use_eng_data')->default(false)->comment('本年度是否提供英文資料');
+            $table->boolean('use_eng_data')->default(true)->comment('本年度是否提供英文資料');
         });
     }
 
